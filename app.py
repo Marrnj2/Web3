97ed2d0fcdf7f5578f67cb79477c36aa7168978e
Nicholas Marr (1000015700) <MARRNJ2@student.op.ac.nz>
2020-03-20 11:35:51 +1300

    Added data sheets and file reading to /home route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from mongoengine import *
 app = Flask(__name__)
+app.config.from_object('config')
 # app.run(host='0.0.0.0', port=80)
 connect('testDB')
 class User(Document):
@@ -13,7 +14,15 @@ class User(Document):
 @app.route('/index')
 @app.route('/')
 def home():
-    User(email="nick.marr@email.com",first_name="Nick",last_name="Marr").save()
+    for file in os.listfir(app.config['FILES_FOLDER']):
+        filename = os.fsdecode(file)
+        path = os.path.join(app.config['FILES_FOLDER'],filename))
+        f = open(path)
+        r = csv.reader(f)
+        d = list(r)
+        for data in d:
+            print(data)
+    #User(email="nick.marr@email.com",first_name="Nick",last_name="Marr").save()
     return render_template("index.html")
 
 
@@ -26,6 +35,16 @@ def insperation():
 def listUsersTest():
     return User.objects.to_json()
 
+@app.route('/users', methods=['GET'])
+@app.route('/users/<user_id>',methods=['GET'])
+def getUsers(user_id=None):
+    users = None
+    if user_id is None:
+        users = User.objects
+    else:
+        users = User.objects.get
+    return users.to_json()
+    
 if __name__ =="__main__":
     app.run(debug=True, port=8080)
 
