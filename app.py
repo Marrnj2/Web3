879039aa16b10dbb372cd1297a209b3ea916bac4
marrnj2 <marrnj2@student.op.ac.nz>
2020-05-25 11:25:56 +1200

    Added code documentation

--- app.py
+++ app.py
@@ -176,9 +176,9 @@ def saveCountry(name):
             country.save()
             return 'Success',201
 
-#if __name__ =="__main__":
-#    app.run(debug=True, port=8080)
-
-
 if __name__ =="__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, port=8080)
+
+
+#if __name__ =="__main__":
+#    app.run(host='0.0.0.0', port=80)
